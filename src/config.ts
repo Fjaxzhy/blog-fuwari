f8357892bfb385225bef2c0ab6a52a5a6446cb9d
KaguraYayoi <kagurayayoi@88.com>
2024-11-11 02:16:33 +0800

    edit. config & post todo.md

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const siteConfig: SiteConfig = {
   banner: {
     enable: true,
     src: 'assets/images/banner.jpg',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    position: 'top',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+    position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
       enable: true,         // Display the credit text of the banner image
       text: 'October 2024 @ Gracile',              // Credit text to be displayed
---
+++ src/content/posts/sitelog/todo.md
@@ -0,0 +1,30 @@
+---
+title: 博客建设待办清单
+published: 2024-11-11
+description: '列举一些待办的工作'
+image: ''
+tags: ["博客", "维护", "日志"]
+category: '站点日志'
+draft: false 
+---
+
+## 准备试着捣腾的玩意
+- [ ] 友链模块
+
+应当在左侧面板加一块，虽然我也没什么朋友（
+
+- [ ] Bangumi Card
+
+番剧详情，来自bangumi
+
+- [ ] Steam Game Card
+
+游戏信息，来自steam
+
+- [ ] Netease Muisc Card
+
+音乐卡片，来自网易云音乐，最好能播放
+
+<hr>
+
+要求是不是有点太多了，有生之年清单（
