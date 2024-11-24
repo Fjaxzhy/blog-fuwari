5626c855e908c67e915229be72cc19a764d37b4e
KaguraYayoi <kagurayayoi@88.com>
2024-11-25 02:24:06 +0800

    update: links

--- src/config.ts
+++ src/config.ts
@@ -100,13 +100,19 @@ export const friendsConfig: FriendsConfig = {
       name: "kihanahare",
       site: "北原花羽栖息地",
       url: "https://kihanahare.github.io/HanahareHeaven/",
-      description: "是网安大佬，wsl",
+      description: "SWPU NSSCTF战队 Web & Reverse 大手子",
     },
     {
       name: "rescld",
       site: "残云的学习笔记",
       url: "https://www.rescld.cn",
       description: "努力考研的舍友",
+    },
+    {
+      name: "snowykami",
+      site: "Snowykami的博客",
+      url: "https://sfkm.me",
+      description: "后端很厉害的轻雪",
     }
   ],
 }
--- src/content/spec/links.md
+++ src/content/spec/links.md
@@ -1,6 +1,12 @@
 # Links
 弥生的朋友们，不分先后
 
-::friend{data="kihanahare|北原花羽栖息地|https://kihanahare.github.io/HanahareHeaven/|是网安大佬，wsl"}
+:::TIP
+网络连通性测试失败则可能需要科学上网喔
+:::
+
+::friend{data="kihanahare|北原花羽栖息地|https://kihanahare.github.io/HanahareHeaven/|SWPU NSSCTF战队 Web & Reverse 大手子"}
 
 ::friend{data="rescld|残云的学习笔记|https://www.rescld.cn|努力考研的舍友"}
+
+::friend{data="snowykami|Snowykami的博客|https://sfkm.me|后端很厉害的轻雪"}
