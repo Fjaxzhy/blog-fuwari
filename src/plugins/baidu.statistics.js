90a906180c828fc6240f071816c8f8bcb0d4b2cf
KaguraYayoi <admin@kagurayayoi.top>
2025-03-11 16:45:57 +0800

    feat. add Baidu Statistics

--- src/plugins/baidu.statistics.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || []
-;(() => {
-  var hm = document.createElement('script')
-  hm.src = 'https://hm.baidu.com/hm.js?86267ea51fd7a88360195e3a0b2285af'
-  var s = document.getElementsByTagName('script')[0]
-  s.parentNode.insertBefore(hm, s)
-})()
